3391ba38ca743e12ca5f02fdf5d65c9916470248
SebasChicaiza <sebastian.erazo275@gmail.com>
2025-05-16 07:53:57 -0500

    Add project files.

---
+++ AccesoDatos/DTO/productoDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos.DTO
+{
+    public class productoDTO
+    {
+        public int idProducto { get; set; }
+        public string? prodNombre { get; set; }
+        public string? prodCategoria { get; set; }
+        public string? prodDescripcion { get; set; }
+
+        public string? prodPrecio { get; set; }
+        public int prodStock { get; set; }
+        public string? prodProveedor { get; set; }
+        public List<string> prodImg { get; set; }
+    }
+}
---
+++ AccesoDatos/tienda.cs
@@ -0,0 +1,45 @@
+﻿using AccesoDatos.DTO;
+using System.Text.Json;
+
+namespace AccesoDatos
+{
+    public class tienda
+    {
+        public string url { get; set; }
+        public string productosUrl { get => url + "api/integracion/productos"; }
+        public async Task<List<productoDTO>> obtenerProductos()
+        {
+            //Creamos un http client para realizar la peticion
+            HttpClient httpClient = new HttpClient();
+
+            try
+            {
+                //Realizamos la peticion a la api
+                var response = await httpClient.GetAsync(productosUrl);
+                //Comprobamos que la respuesta es correcta
+                response.EnsureSuccessStatusCode();
+                //Leemos el contenido de la respuesta
+                var jsonString = await response.Content.ReadAsStringAsync();
+                //Deserializamos el contenido a una lista de productos
+                var res = JsonSerializer.Deserialize<List<productoDTO>>(jsonString, new JsonSerializerOptions { PropertyNameCaseInsensitive=true});
+                foreach (var item in res)
+                {
+                    if(string.IsNullOrEmpty(item.prodCategoria))
+                    {
+                        item.prodCategoria = "NA";
+                    }
+                    if (string.IsNullOrEmpty(item.prodNombre))
+                    {
+                        item.prodCategoria = "NA";
+                    }                    
+                }
+                return res?? new List<productoDTO>();
+
+            }
+            catch (Exception ex)
+            {
+                return new List<productoDTO>();
+            }
+        }
+    }
+}
