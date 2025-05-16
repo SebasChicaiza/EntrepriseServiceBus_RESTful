789f3411683d1a39fb1f9cfef7609faeb4e2bb63
SebasChicaiza <sebastian.erazo275@gmail.com>
2025-05-16 08:19:30 -0500

    Creacion de APIs RESTful y logica de negocio

--- Datos/datosProducto.cs
+++ Datos/datosProducto.cs
@@ -1,7 +1,81 @@
-﻿namespace Datos
+﻿using System.Text;
+using System.Threading.Tasks;
+using AccesoDatos;
+using AccesoDatos.DTO;
+
+namespace Datos
 {
     public class datosProducto
     {
+        private static datosProducto _instance;
+        private static readonly object _lock = new object();
+        private List<productoDTO> _productoCache;
+        private static List<tienda> tiendas = new List<tienda>()
+        {
+            new tienda()
+            {
+                url="http://backendpawstails.runasp.net"
+            },
+            new tienda()
+            {
+                url="http://backendrestfableedsteel.runasp.net"
+            },
+            new tienda()
+            {
+                url="http://backendpeteats.runasp.net/"
+            }
+        };
+        private datosProducto()
+        {
+            _productoCache = new List<productoDTO>();
+        }
+        public static datosProducto Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new datosProducto();
+                    }
+                    return _instance;
+                }
+            }
+        }
+        public async Task<List<productoDTO>> obtenerProductos()
+        {
+            if (_productoCache.Any())
+            {
+                return _productoCache;
+            }
+            _productoCache = new List<productoDTO>();
+            foreach (var tienda in tiendas)
+            {
+                var productos = await tienda.obtenerProductos();
+                if (productos != null && productos.Count > 0)
+                {
+                    _productoCache.AddRange(productos);
+                }
+            }
+            return _productoCache.ToList();
+        }
+        public async Task<List<productoDTO>> ObtenerTodosLosProductos()
+        {
+            if (_productoCache.Any())
+            {
+                return _productoCache;
+            }
 
+            _productoCache = new List<productoDTO>();
+            foreach (var tienda in tiendas)
+            {
+                List<productoDTO> productosDeApi = await tienda.obtenerProductos();
+                _productoCache.AddRange(productosDeApi);
+            }
+
+            return _productoCache.ToList(); // Devolvemos una nueva lista para evitar modificaciones externas
+        }
     }
+
 }
---
+++ Logica/logicaProductos.cs
@@ -0,0 +1,20 @@
+﻿using AccesoDatos.DTO;
+using Datos;
+
+namespace Logica
+{
+    public class logicaProductos
+    {
+        private readonly datosProducto _datosProducto = datosProducto.Instance;
+        public async Task<List<productoDTO>> obtenerProductos()
+        {
+            return await _datosProducto.ObtenerTodosLosProductos();
+        }
+        public async Task<List<productoDTO>> obtenerProductosPorNombre(string nombre)
+        {
+            List<productoDTO> res = await _datosProducto.ObtenerTodosLosProductos();
+            return res.Where(c => c.prodNombre.Contains(nombre)).ToList();
+        }
+
+    }
+}
